2531793d15959536ed7eec90f0d1bf9da005857c
Jaysig <jasontsigmon@gmail.com>
2015-11-17 17:16:29 -0500

    set up google polymer

---
+++ public/extension/popup.js
@@ -0,0 +1,13 @@
+//Check if Incognito
+
+//Check if Signed In
+
+//On click scrape the page for data
+  //chrome browser action onclicked listener
+  //Active Tab run script
+
+// Send Data to server
+
+//Create Bookmark
+  //bookmarks.create
+  //bookmarks.update
