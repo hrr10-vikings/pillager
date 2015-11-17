92ab12059b85dabb638b341a19a559cad439a5c4
amenotu <kyabut@gmail.com>
2015-11-17 12:31:37 -0800

    merging

--- server/server.js
+++ server/server.js
@@ -12,13 +12,6 @@ app.get('/', function (req, res) {
 app.use(bodyParser);
 =======
 app.use(express.static(__dirname + '/public'));
-<<<<<<< 8409fd96aad18efa57526d913ef34a135e829997
-=======
-
-var server = app.listen(app.get('port'), function () {
-  var host = server.address().address;
-  var port = server.address().port;
->>>>>>> server.js now serves static files
 
 var server = app.listen(app.get('port'), function () {
   var host = server.address().address;
