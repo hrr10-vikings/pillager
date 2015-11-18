796a14f6c5386c156c5f042901f2c57826d18610
Jaysig <jasontsigmon@gmail.com>
2015-11-17 20:04:39 -0500

    working on popup.js

--- public/extension/popup.js
+++ public/extension/popup.js
@@ -1,13 +1,23 @@
 //Check if Incognito
+function base(tab,data){
+  if(tab.incognito){
+    //change the popup body
+  } else{
+    //check for logged in
+    chrome.identify.launchWebAuthFlow{
+      {'url': '<url-to-do-auth>', 'interactive': true},
+      function(redirect_url) { /* Extract token from redirect_url */ });
+    }
+    if(/* loggined in*/){
+      //On click scrape the page for data
+        //chrome browser action onclicked listener
+        //Active Tab run script
+    } else{
 
-//Check if Signed In
+    }
+  }
+}
 
-//On click scrape the page for data
-  //chrome browser action onclicked listener
-  //Active Tab run script
-
-// Send Data to server
-
-//Create Bookmark
-  //bookmarks.create
-  //bookmarks.update
+var pillage(tab,data){
+  // Send Data to server
+}
