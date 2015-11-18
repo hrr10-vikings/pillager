9110cb86886f9c5b348ecdb33384ce676813ca00
Ben Pochily <ben.pochily@gmail.com>
2015-11-18 15:09:43 -0500

    server API seems to work

--- server/apiControllers.js
+++ server/apiControllers.js
@@ -1,26 +1,23 @@
 var alchemy = require('./alchemy');
 var db = require('./fireBaseAPI');
 var auth = require('./auth');
-var NUM_KEYWORDS = 10;
+var NUM_KEYWORDS = 4;
 
 module.exports.parseUrl = function(req, res) {
   alchemy.getKeywords(req.body.newUrl, NUM_KEYWORDS, function(results) {
-    db.addBookmark("ben", req.body.newUrl, results);
-    db.getAllBookmarks("ben", function (sites) {
-      res.end(JSON.stringify(sites));
-    });    
+    db.addBookmark(req.user, req.body.newUrl, results);
+    res.end(JSON.stringify(results)); 
   });
 }
 
 module.exports.getUrls = function(req, res) {
-  console.log(req.body);
-  var dummyUrls = {'amazon': ['keyword', 'keyword2'], 'google': ['keyword3', 'keyword4']};
-  res.json(dummyUrls);
+  db.getAllBookmarks(req.user, function (sites) {
+    res.end(JSON.stringify(sites));
+  });    
 }
 
 module.exports.signIn = function(req, res, next) {
-  console.log('sign in');
-  res.end('please go away');
+  auth.signIn(req, res, next);
 }
 
 module.exports.signUp = function(req, res, next) {
--- server/auth.js
+++ server/auth.js
@@ -1,39 +1,34 @@
 var jwt = require('jwt-simple');
 var bcrypt = require('bcrypt-nodejs');
 var db = require('./fireBaseAPI');
-var SECRET = 'secret';
+var SECRET = process.env.JWTSecret || require('./config').JWTSecret;
 
 module.exports.checkAuth = function(req, res, next) {
-  console.log("in auth");
-  console.log(req.headers);
   var token = req.headers['x-access-token'];
-  console.log(token);
   if (!token) {
       return res.sendStatus(403); // send forbidden if a token is not provided
   }
 
   try {
-    // decode token and attach user to the request
-    // for use inside our controllers
     req.user = jwt.decode(token, SECRET);
     next();
     } catch(error) {
-      return next(error);
+      res.sendStatus(403);
     }
 };
 
 module.exports.signIn = function (req, res, next) {
     var username = req.body.username, password = req.body.password;
     db.getUser(username, function(hash) {
-      bcrypt.compare(password, hash, function(err, res) {
-        if (res) {
+      bcrypt.compare(password, hash, function(err, result) {
+        if (result) {
           res.json({token: jwt.encode(username, SECRET)});
           next();
         } else {
           res.sendStatus(401);
         }
-      }, function() {res.sendStatus(401);});
-    });
+      });
+    }, function() {res.sendStatus(401);});
   };
 
 module.exports.signUp = function (req, res, next) {
