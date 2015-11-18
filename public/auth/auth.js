48fa41ab59a6bbc40a2a8c4e130b4d47634c68d1
amenotu <kyabut@gmail.com>
2015-11-17 21:23:33 -0800

    Added files to set up the frontend of the app

---
+++ public/auth/auth.js
@@ -0,0 +1,7 @@
+//angular module that will take care of authentication on the frontend
+angular.module('pillager.auth', [])
+
+.controller('AuthController', function ($scope) {
+  //some code here...
+});
+
