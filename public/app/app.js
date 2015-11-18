ceb6f4dd7cc83dfeafae15974f24720b7a0b90d6
amenotu <kyabut@gmail.com>
2015-11-18 13:48:24 -0800

    client able to send POST requests to server

--- public/app/app.js
+++ public/app/app.js
@@ -1,15 +1,19 @@
-var app = angular.module('pillager', ['ngRoute']);
+var app = angular.module('pillager', [
+    'pillager.services', 
+    'pillager.auth',
+    'ngRoute'
+]);
 
 //frontend routing and redirects using ngRoute
 app.config(function ($routeProvider) {
   $routeProvider
     .when('/signin', {
     	templateUrl: 'auth/signin.html',
-    	//controller: 'AuthController'
+    	controller: 'AuthController'
     })
     .when('/signup', {
     	templateUrl: 'auth/signup.html',
-    	//controller: 'AuthController'
+    	controller: 'AuthController'
     })
     .otherwise({
     	redirectTo: '/signin'
--- public/auth/auth.js
+++ public/auth/auth.js
@@ -1,7 +1,14 @@
 //angular module that will take care of authentication on the frontend
 angular.module('pillager.auth', [])
 
-.controller('AuthController', function ($scope) {
-  //some code here...
-});
+.controller('AuthController', function ($scope, Authorize) {
+  $scope.user = {};
 
+  $scope.signin = function () {
+    Authorize.signin($scope.user);
+  }
+
+  $scope.signup = function () {
+  	Authorize.signup($scope.user);
+  }
+})
