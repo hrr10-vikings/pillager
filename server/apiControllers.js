3ce3f3f0b6008840ca54e14577823cdadb070f20
Ben Pochily <ben.pochily@gmail.com>
2015-11-19 15:10:23 -0500

    restore cross-origin handling

--- server/apiControllers.js
+++ server/apiControllers.js
@@ -21,10 +21,8 @@ module.exports.getUrls = function(req, res) {
   });
 };
 
-module.exports.signIn = function(req, res) {
-  module.exports.signIn = function(req, res, next) {
+module.exports.signIn = function(req, res, next) {
     auth.signIn(req, res, next);
-  }
 };
 
 module.exports.signUp = function(req, res, next) {
--- server/apiRouter.js
+++ server/apiRouter.js
@@ -4,7 +4,7 @@ var auth = require('./auth.js');
 module.exports = function(app) {
 
   //allow cross-origin requests so we can communicate with Chrome extension
-  /*app.all("/*", function(req, res, next) {
+  app.all("/*", function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
@@ -15,7 +15,7 @@ module.exports = function(app) {
     return next();
   }
   return res.send(204);
-  });*/
+  });
   
   app.use('/urls', auth.checkAuth); //hijack all requests to /url and check authentication
   
