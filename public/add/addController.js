2cc6759e049d35c619c38b7049ef1adb3ffd4760
pbhorjee <paul@bhorjee.com>
2015-11-19 20:15:59 -0600

    Add 'add' feedback

--- public/add/addController.js
+++ public/add/addController.js
@@ -5,17 +5,26 @@ angular.module('pillager.add', [])
   $scope.tags = '';
   
   $scope.add = function () {
+    var url = $scope.url;
     var toSend = {newUrl: $scope.url, tags: _.map($scope.tags.split(','), function(str) {return str.trim();})}
     $scope.url = '';
     $scope.tags = '';
-    console.log(toSend);
+    //console.log(toSend);
     $http({
       method: 'POST',
       url: '/api/urls',
-      data: toSend,
+      data: toSend
     }).then(function(res) {
+      $("#added").hide();
+      $("#url-added").text("Site Bookmarked!  " + url);
 
+      var newTags = 'Tags:<br>';
+      for (var i = 0; i < res.data.length; i++) {
+        newTags += res.data[i] + '<br>';
+      }
+      $("#tags-added").html(newTags);
 
+      $("#added").show("fast");
       console.log(res);
     })
   };
